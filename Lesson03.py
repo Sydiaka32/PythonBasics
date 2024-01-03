bd3ff1d891545ceae531ff8f08ebd0576f923351
Sydiaka32 <max.sydiaka@gmail.com>
2024-01-03 21:46:35 +0200

    Implemented converting temperature from Celsius to Farengate

--- Lesson03.py
+++ Lesson03.py
@@ -11,3 +11,8 @@ user_age = input("What age will you be this year?: ")
 current_year = datetime.datetime.now().year
 born_year = current_year - int(user_age)
 print(f"You were born in {born_year}")
+
+#Task3
+temp_C = input("Enter current temperature in Celsius: ")
+temp_F = (int(temp_C) * 9/5) + 32
+print(f"You temperature in Farengate is {temp_F}")
