d8cc7855984efc6690a942f5fc5e6c0193938134
Sydiaka32 <max.sydiaka@gmail.com>
2024-01-13 17:08:18 +0200

    Change structure of validation list + add validation on empty input

--- Homework/HomeWork05.py
+++ Homework/HomeWork05.py
@@ -3,16 +3,25 @@ from typing import List
 #1 Свторіть функцію, яка приймає список чисел і повертає список їх квадратів
 
 def get_user_input() -> List[str]:
-    input_str = input("Input list of numbers devided by space: ")
+    input_str = input("Input list of numbers divided by space: ")
     return input_str.split(" ")
 
 def validate_list(input_list: List[str]) -> List[int]:
-    try:
-        valid_list = [int(el) for el in input_list]
-        return valid_list
-    except ValueError as err:
-        print(err)
-        raise TypeError("Please enter only numbers")
+    valid_list = []
+
+    for el in input_list:
+        if not el.strip():
+            raise TypeError("Empty input detected. Please enter valid numbers.")
+
+        try:
+            num = int(el)
+            valid_list.append(num)
+        except ValueError as err:
+            print(f"Invalid input for element '{el}': {err}")
+            raise TypeError("Invalid input detected. Please enter only numbers")
+
+    return valid_list
+
 
 def squared_list(valid_list: List[int]) -> List[int]:
     result = [num ** 2 for num in valid_list]
