bb1e08071048c6e500b34fbc7835dc07d9b3426e
Sydiaka32 <max.sydiaka@gmail.com>
2024-01-25 21:05:49 +0200

    second code with decorator

--- Lections/Lection08.py
+++ Lections/Lection08.py
@@ -1,16 +1,28 @@
+from typing import List
 
-def log(add):
-    def wrapped(a: int, b:int):
-        result = add(a, b)
-        with open("file.txt", "w") as f:
+
+def logger(func):
+    def wrapped(num_list: List):
+        result = func(num_list)
+        with open("decorator.txt", "w") as f:
             f.write(str(result))
         return result
     return wrapped
 
 
-def add(a: int, b:int) -> int:
-    res = a + b
-    return res
+def summator(num_list: List) -> int:
+    return sum(num_list)
 
 
-add = log(add)
+summator = logger(summator)
+
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    print(summator.__name__)
+    result = summator(my_list)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
