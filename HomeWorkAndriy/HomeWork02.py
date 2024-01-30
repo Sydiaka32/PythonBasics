ca7913b02b98ca218c28db188bd2ccafa0019ca8
Sydiaka32 <max.sydiaka@gmail.com>
2024-01-30 21:40:07 +0200

    add main function which solve all cases

--- HomeWorkAndriy/HomeWork02.py
+++ HomeWorkAndriy/HomeWork02.py
@@ -14,6 +14,7 @@
 list = [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
 print("Вихідний массив: ", list)
 
+
 #1.1)
 
 def duplicate(list):
@@ -23,7 +24,8 @@ def duplicate(list):
             correct_list.append(num)
     return correct_list
 
-print("Масив без дублікатів: ", duplicate(list))
+
+
 
 #1.2)
 
@@ -31,7 +33,8 @@ def get_top(list):
     top_three = sorted(list, reverse=True)[:3]
     return top_three
 
-print("Топ три значення: ", get_top(list))
+
+
 
 #1.3)
 
@@ -39,14 +42,28 @@ def get_min_index(list):
     min_index = list.index(min(list))
     return min_index
 
-print("Індекс найменшого числа: ", get_min_index(list))
 
 #1.4)
+
 
 def reverse_list(list):
     reversed_list = list[::-1]
     return reversed_list
 
-print("Перевернутий масив: ", reverse_list(list))
+
+def main():
+    correct_list = duplicate(list)
+    print("Масив без дублікатів: ", correct_list)
+
+    reversed_list = reverse_list(correct_list)
+    print("Перевернутий масив: ", reversed_list)
+
+    min_index = get_min_index(reversed_list)
+    print("Індекс найменшого числа: ", min_index)
+
+    top_three = get_top(reversed_list)
+    print("Топ три значення: ", top_three)
 
 
+if __name__ == '__main__':
+    main()
