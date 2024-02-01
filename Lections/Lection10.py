ce9e815323d0ca568ba9dcbe97768b165dd22d37
Sydiaka32 <max.sydiaka@gmail.com>
2024-02-01 22:31:47 +0200

    final

--- Lections/Lection10.py
+++ Lections/Lection10.py
@@ -1,4 +1,31 @@
+from abc import ABC, abstractmethod
 
+class Enemy(ABC):
+
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def attack(self):
+        pass
+
+    @abstractmethod
+    def do_bad_things(self):
+        pass
+
+
+class Skeleton(Enemy):
+
+    def attack(self):
+        print("Skeleton hit by sword")
+
+    def move(self):
+        print("Move it")
+
+    def do_bad_things(self):
+        print("Bad things")
 class Dog:
     def __init__(self, name: str):
         self.name = name
@@ -11,17 +38,27 @@ class Dog:
         return f"Dog with name = {self.name} and age = {self.age}"
 
 
+class Puppy(Dog):
+    def voice(self):
+        print("Тяф тяф")
+
 class Korgi(Dog):
     pass
 
 def main():
-    my_dog = Dog(name="Шарик")
-    my_dog.voice()
+    #my_dog = Dog(name="Шарик")
+    #my_dog.voice()
 
     #print(my_dog)
 
-    korgi = Korgi(name="Korgi")
-    print(korgi)
+    #korgi = Korgi(name="Korgi")
+    #print(korgi)
+
+    #sk = Skeleton()
+    #sk.attack()
+
+    puppy = Puppy(name="112")
+    puppy.voice()
 
 
 if __name__ == '__main__':
